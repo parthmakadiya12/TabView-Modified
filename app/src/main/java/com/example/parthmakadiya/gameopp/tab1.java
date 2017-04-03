6f963e793b7e9c5dac51c5d52d0071a2d624439a
Parth Makadiya <parthmakadiya12@gmail.com>
2017-04-03 23:41:20 +0530

    first commit

---
+++ app/src/main/java/com/example/parthmakadiya/gameopp/tab1.java
@@ -0,0 +1,20 @@
+package com.example.parthmakadiya.gameopp;
+
+/**
+ * Created by Parth Makadiya on 4/3/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/parthmakadiya/gameopp/tab2.java
@@ -0,0 +1,20 @@
+package com.example.parthmakadiya.gameopp;
+
+/**
+ * Created by Parth Makadiya on 4/3/2017.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+}
